ca8668c84bb5f40e9c9ce415d268537e2b5403f7
jbericks <jbericks@users.noreply.github.com>
2014-05-09 06:20:35 -0700

    Create adjustLDAPTime.sh
    This script adjusts LDAP timestamps (yyyyMmddhhmmssZ) based on the /bin/date conversion string that is input.

---
+++ adjustLDAPTime.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Description   : This script adjusts LDAP timestamps (yyyyMmddhhmmssZ) based on the /bin/date conversion string that is input.
+# Usage		: ./adjustLDAPTime.sh <LDAP timestamp> <string>  |  ./adjustLDAPTime.sh 20150101070000Z "180 days"
+
+
+timestamp=$1
+#Convert LDAP timestamp to a date that /bin/date can parse:
+CONVERSION=`printf "%s\n" "${timestamp:0:4}-${timestamp:4:2}-${timestamp:6:2} ${timestamp:8:2}:${timestamp:10:2}:${timestamp:12:2}"`
+#Convert the date back to LDAP format, with the adjustment:
+/bin/date -u -d "$CONVERSION $2 $3 $4" +%Y%m%d%H%M%SZ
