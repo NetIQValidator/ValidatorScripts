7edf97a803acb39de6b1885848c1363db0f0c32d
Lothar Haeger <lothar.haeger@brummelhook.com>
2015-09-04 19:48:29 +0200

    Moved ValidatorLauncher to it's won repo

--- ValidatorLauncher/source/compile.cmd
+++
@@ -1,4 +0,0 @@
-@Echo To run and compile AU3 scripts, download and install AutoIt v3.3.8.1+ from www.autoitscript.com...
-@Echo.
-"C:\Program Files (x86)\AutoIt3\Aut2Exe\Aut2exe.exe" /in validator.au3 /out ../validator.exe /icon validator.ico /x86
-@Pause
